a7280bc7cee26765ae8201f1ac40b9d93ff423b0
Blanco237 <asongrandy9@gmail.com>
2022-06-03 10:14:14 +0100

    Completed Tailwind Installation

--- tailwind-react/src/App.js
+++ tailwind-react/src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    
-    <div className="text-center">
+    <div className="App">
       <header className="App-header">
-        <img src={logo} className="h-3/4 " alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
