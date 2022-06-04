87dd1392e92f1b9689992ec2c6f4a7cf9fa4d54e
Blanco237 <asongrandy9@gmail.com>
2022-06-04 18:05:00 +0100

    Configured colors and added source images

--- tailwind-react/tailwind.config.js
+++ tailwind-react/tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 20s linear infinite'
+      },
+      colors: {
+        'red': '#ff525d',
+        'light-red': '#ff7a85',
+        'dark-blue': '#1f3f5b',
+        'white': '#ffffff',
+        'gray-blue': '#c8c8cb',
+        'gray-dark': '#4b5862',
+        'deep-dark': '#25252d',
+        'grad-red': '#ff8f70',
+        'grad-light-red': '#ff3d54',
+        'grad-blue': '#1fa1f2',
+        'grad-dark-blue': '#00d8d8',
+      },
+    },
+    screens: {
+      'mobile': '375px',
+      'desktop': '1024px'
+    },
   },
   plugins: [],
 }
