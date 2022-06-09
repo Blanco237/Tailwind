e1fd67aa9d270a8a0a07ba18e9e47453e3fa6c19
Blanco237 <asongrandy9@gmail.com>
2022-06-09 22:23:26 +0100

    Added responsivity to section three

--- tailwind-react/src/components/SectionThree.jsx
+++ tailwind-react/src/components/SectionThree.jsx
@@ -10,11 +10,11 @@ const SectionThree = () => {
     }
 
   return (
-    <section className='flex gap-8 bg-svg3 p-11 mt-10 h-[27rem] justify-between items-center rounded-bl-[10rem] rounded-tr-[10rem]'>
-        <section name="image-container" className='relative top-0 left-0 w-[40%] flex flex-col items-center justify-end mt-[8rem]' >
-            <img src={phones} className="w-[120rem]"/>
+    <section className='flex md:flex-row flex-col gap-8 bg-svg3 p-11 md:mt-10 mt-32 md:h-[27rem] justify-between items-center rounded-bl-[10rem] rounded-tr-[10rem]'>
+        <section name="image-container" className='relative top-0 left-0 md:w-[40%] w-full flex flex-col items-center justify-end md:mt-[8rem] mt-[-10rem]' >
+            <img src={phones} className="md:w-[120rem] w-full"/>
         </section>
-        <section name="content-container" className='w-[50%] px-12'>
+        <section name="content-container" className='md:w-[50%] md:px-12 w-full px-4 text-center md:text-left'>
             <Article title={data.title} body={data.body} color='white' />
         </section>
     </section>
