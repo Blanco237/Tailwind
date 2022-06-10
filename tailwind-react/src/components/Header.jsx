ded9c3860118feb8d9b597e0f29980ff8d984682
Blanco237 <asongrandy9@gmail.com>
2022-06-10 20:19:00 +0100

    Attempted responsive header

--- tailwind-react/src/components/Header.jsx
+++ tailwind-react/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import logo from '../assets/images/logo.svg'
 import icon from '../assets/images/icon-hamburger.svg'
@@ -7,11 +7,15 @@ import icon from '../assets/images/icon-hamburger.svg'
 import DropdownItem from '../partials/DropdownItem'
 
 const Header = () => {
+
+
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <header className=' absolute top-0 left-0 w-full bg-transparent items-center flex flex-row justify-around gap-4 py-4 pt-14'>
+    <header className='absolute top-0 left-0 w-full bg-transparent items-center flex flex-row justify-around gap-4 py-4 pt-14'>
         <img src={logo} className='w-max' />
-        <nav>
-            <ul className='hidden flex-row items-center gap-4 text-white justify-between text-lg md:flex '>
+        <nav className={`${showMenu? 'show':'hide'} mb-[-12rem] ml-[-3.5rem] show-bg`}>
+            <ul className=' flex-col md:flex-row items-center gap-4 z-10 md:text-white text-dark-blue justify-between md:text-lg md:flex text-2xl '>
                 { [ {title: 'Product', links: ['Overview','Pricing','Marketplace','Features','Integrations']},
                     {title: 'Company', links: ['About','Team','Careers','Blog',]},
                     {title: 'Connect', links: ['Contact Us','Newsletter','LinkedIn']},
@@ -19,11 +23,11 @@ const Header = () => {
                    ) }
             </ul>
         </nav>
-        <nav className='text-white gap-4 text-lg cursor-pointer hidden md:flex'>
+        <nav className={`${showMenu? 'show':'hide'} md:text-white text-dark-blue gap-4 md:text-lg text-2xl cursor-pointer relative md:flex md:flex-row flex-col mb-[-30rem] ml-[-14rem] z-[5] spacing`}>
         <button className=' px-5 py-1'>Login</button>
             <button className='rounded-3xl px-5 py-2 transition duration-300 ease-linear bg-white text-light-red font-semibold hover:bg-light-red hover:text-white'>Sign Up</button>
         </nav>
-        <img src={icon} className='w-max flex md:hidden' />
+        <img src={icon} className='w-max flex md:hidden' onClick={() => setShowMenu(!showMenu)}/>
     </header>
   )
 }
